d5edded7144d71ca421910ddbb90f9c7a90f5ebd
T0person <59031052+T0person@users.noreply.github.com>
2020-07-08 01:03:11 +0300

    create Zoo
    Добавлен класс животного, имя, тип и голос

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Zoo
+{
+
+    class Ram
+    {
+        private string type = "Баран", say = "бе-е-е-е", name;
+
+        public Ram(string name)
+        {
+            this.name = name;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"{type} {name}: {say}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ram ram1 = new Ram("Степа");
+            Ram ram2 = new Ram("Вася");
+            Ram ram3 = new Ram("Миша");
+            Ram ram4 = new Ram("Гена");
+
+            ram1.GetInfo();
+            ram2.GetInfo();
+            ram3.GetInfo();
+            ram4.GetInfo();
+
+            Console.WriteLine("Введи любой символ");
+            Console.ReadKey();
+
+        }
+    }
+}
